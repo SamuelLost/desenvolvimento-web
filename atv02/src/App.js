48225efc24eeed67a5fd0b64cdbd2e772c0b3282
Samuel Henrique <samuelhenriqec@alu.ufc.br>
2022-09-19 18:41:06 -0300

    atividade 2 - vota cidades

---
+++ atv02/src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+import { VotaCidades } from './components/estados/VotaCidades';
+function App() {
+  return (
+    <div className="App">
+      <VotaCidades/>
+    </div>
+  );
+}
+
+export default App;
---
+++ atv02/src/components/estados/VotaCidades.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+export const VotaCidades = () => {
+    const [qxd, setQxd] = useState(0)
+    const [lim, setLim] = useState(0)
+    const [crateus, setCrateus] = useState(0)
+
+    const reset = () => {
+        setCrateus(0);
+        setLim(0);
+        setQxd(0);
+    }
+    const fim = () => {
+        let aux = Math.max(qxd, lim, crateus)
+        if((aux === qxd && aux === lim) || (aux === crateus && aux === lim) || (aux === crateus && aux === qxd)) {
+            alert("Houve empate")
+        } else if(aux === lim) {
+            alert("Limoeiro venceu")
+        } else if(aux === crateus) {
+            alert("Crateús venceu")
+        } else {
+            alert("Quixadá venceu")
+        }
+    }
+    return (
+        <div>
+            <div>
+                <h2>Quixadá: {qxd}</h2>
+                <h2>Limoeiro do Norte: {lim}</h2>
+                <h2>Crateús: {crateus}</h2>
+            </div>
+            <button onClick={() => setQxd(ant => ant +1)}>Votar Quixadá</button> <br />
+            <button onClick={() => setLim(ant => ant +1)}>Votar Limoeiro do Norte</button> <br />
+            <button onClick={() => setCrateus(ant => ant +1)}>Votar Crateús</button> <br />
+            <button onClick={() => reset()}>Resetar</button> <br />
+            <button onClick={() => fim()}>Fim da votação</button> <br />
+        </div>
+    )
+}
